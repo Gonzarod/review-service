8c9f663a1ed58ee58a89d12abc345bf90c1463da
Jesus Duran Huancas <jesusgonzaloduran@gmail.com>
2020-10-30 02:35:35 -0500

    Eureka client config

--- src/main/java/com/evertix/reviewservice/ReviewServiceApplication.java
+++ src/main/java/com/evertix/reviewservice/ReviewServiceApplication.java
@@ -1,14 +1,17 @@
 package com.evertix.reviewservice;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
